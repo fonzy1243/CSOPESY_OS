3ff1c43a580c2227117a02ec9585d72dfc291035
Fonzy <fonzy.fermin@gmail.com>
2025-06-25 00:09:18 +0800

    Implement subtract instruction

--- src/process/instruction.cpp
+++ src/process/instruction.cpp
@@ -53,7 +53,34 @@ std::string AddInstruction::get_type_name() const
 
 void SubtractInstruction::execute(Process &process)
 {
+    size_t var1_address = process.memory->get_var_address(process.symbol_table, var1);
 
+    std::string val2_str, val3_str;
+
+    if (use_val2) {
+        val2_str = std::to_string(val2);
+    } else {
+        const size_t var2_address = process.memory->get_var_address(process.symbol_table, var2);
+        val2 = process.memory->read_word(var2_address);
+        val2_str = std::format("{}({})", var2, val2);
+    }
+
+    if (use_val3) {
+        val3_str = std::to_string(val3);
+    } else {
+        const size_t var3_address = process.memory->get_var_address(process.symbol_table, var3);
+        val3 = process.memory->read_word(var3_address);
+        val3_str = std::format("{}({})", var3, val3);
+    }
+
+    uint16_t result;
+    if (val2 >= val3) {
+        result = val2 - val3;
+    } else {
+        result = 0;
+    }
+
+    process.memory->write_word(var1_address, result);
 }
 
 std::string SubtractInstruction::get_type_name() const
